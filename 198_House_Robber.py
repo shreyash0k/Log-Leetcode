42ab1533dfb24ad2a905a3965837fd6a9fff0fe3
shreyash0k <karandikarshreyash@gmail.com>
2026-01-06 01:25:45 -0500

    198_House_Robber

---
+++ 198_House_Robber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def helper(i):
+            if i>=len(nums):
+                return 0
+            
+            if i in memo:
+                return memo[i]
+
+            rob_current = nums[i] + helper(i+2)
+            
+            skip_current = helper(i+1)
+
+            memo[i] = max(rob_current,skip_current)
+
+            return memo[i]
+        
+        return helper(0)
