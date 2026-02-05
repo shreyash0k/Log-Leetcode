253025987a27dff60277f5142d352fceebf988b3
Shreyash <karandikarshreyash@gmail.com>
2026-02-04 23:42:44 -0500

    solve number_of_island problem using both dfs and bfs

--- 200_Number_of_islands.py
+++ 200_Number_of_islands.py
@@ -1,43 +1,70 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # calculate rows cols count and numberOfIslands 
+        
+        count = 0 
         rows = len(grid)
         cols = len(grid[0])
-        numOfIsland = 0 
 
-        def findIsland(r,c):
-            queue = deque()
-            queue.append((r,c))
-            grid[r][c] = '0'
+        def bfs(row,col):
+            grid[row][col] = "0"
+            queue = deque([(row,col)])
+            directions = [(0,-1),(0,1),(-1,0),(1,0)]
             while queue:
-                row,col = queue.popleft()
-                # visit left 
-                if (row-1>=0 and grid[row-1][col] =='1'):
-                    grid[row-1][col] = '0'
-                    queue.append((row-1,col))
-
-                # visit right
-                if (row+1<rows and grid[row+1][col]=='1'):
-                    grid[row+1][col] = '0'
-                    queue.append((row+1,col))
-
-                # visit bottom 
-                if (col+1<cols and grid[row][col+1] == '1'):
-                    grid[row][col+1] = '0'    
-                    queue.append((row,col+1))
-            
-                # visit top 
-                if (col-1>=0 and grid[row][col-1] == '1'):
-                    grid[row][col-1] = '0'      
-                    queue.append((row,col-1))
+                r,c = queue.popleft()
+                for dr,dc in directions:
+                    new_row = r+dr
+                    new_col = c+dc
+                    if (0<= new_row <rows and 0<= new_col<cols and grid[new_row][new_col]=="1"):
+                        grid[new_row][new_col] = "0"
+                        queue.append((new_row,new_col))  
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col]=='1':
-                    findIsland(row,col)
-                    numOfIsland+=1 
-        
-        return numOfIsland 
+                if (grid[row][col]=="1"):
+                    count+=1
+                    bfs(row,col)
 
-# time complexity - o(row * col)
-# space complexity - o(row * col)
+        return count                 
+        # go through each cell of grid 
+        # if cell = 1 increment count by 1  
+        # run bfs / dfs to make all its adjacent lands as 0 
+        #   bfs(row,col)
+        #   
+        # return count 
+
+
+# time complexity 
+# we are visiting each cell so m*n 
+# while visiting first cell we might come across island made of full matrix so worst case m*n 
+# total time complexity O(m+n) + O(m+n) = O(m+n)
+
+# space complexity 
+# we might have to store full matrix in queue. so worst case m*n
+
+
+# dfs solution 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        count = 0 
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(row,col):
+            grid[row][col] = "0"
+            directions = [(0,-1),(0,1),(-1,0),(1,0)]
+
+            for dr, dc in directions:
+                new_row = dr+row
+                new_col = dc+col
+                if (0<=new_row<rows and 0<=new_col<cols and grid[new_row][new_col]=="1"):
+                    dfs(new_row,new_col)
+            
+
+        for row in range(rows):
+            for col in range(cols):
+                if (grid[row][col]=="1"):
+                    count+=1
+                    dfs(row,col)
+
+        return count
