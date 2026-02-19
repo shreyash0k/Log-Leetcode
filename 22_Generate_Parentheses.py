69deeca3761ad687c42f94293236b2e77596ea7b
Shreyash <karandikarshreyash@gmail.com>
2026-02-19 17:47:47 -0500

    Refactor generateParenthesis function for clarity and efficiency

--- 22_Generate_Parentheses.py
+++ 22_Generate_Parentheses.py
@@ -1,30 +1,28 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+    
         result = []
-        def dfs(substring, openParentheses, closeParentheses):
-            if (openParentheses == n and closeParentheses == n):
-                result.append("".join(substring))
-                return
-            
-            if openParentheses < n:
-                substring.append("(")
-                dfs(substring, openParentheses+1, closeParentheses)
-                substring.pop()
 
-            if closeParentheses < openParentheses and closeParentheses < n:
-                substring.append(")")
-                dfs( substring, openParentheses, closeParentheses+1)
-                substring.pop()
-        
-        dfs([],0,0)
+        def dfs(opening, closing, substr):
+            if opening == n and closing == n :
+                result.append(substr)
+                return 
+            if opening<n:
+                dfs(opening+1, closing, substr+"(")
+            if closing<n and opening>closing:
+                dfs(opening,closing+1, substr+")")
+                
+        dfs(0,0,"")
         return result
+
     
 
-# time complexity - 2 choices at 2n levels so 2^2n = 4 ^n 
-# at each leaf node we can make copy. copy operation takes O(n) time.
-# total time = (n*4^n)
+# O(4^n)        
+# O(n)
+                            
 
-# space complexity 
-# Number of results ≈ 4^n (upper bound)
-# Length of each = 2n = O(n)
-# Total output space = O(n × 4^n)
+
+        
+        
+
+    
