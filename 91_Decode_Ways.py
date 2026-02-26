894e95c40444f495221a1958c41e176d423cf0be
Shreyash <karandikarshreyash@gmail.com>
2026-02-26 00:48:00 -0500

    refactor decoding logic in numDecodings for improved clarity and efficiency

--- 91_Decode_Ways.py
+++ 91_Decode_Ways.py
@@ -1,20 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n = len(s)
-        memo = {n:1}
+        memory = {len(s):1}
+        def decode(i):
+            if s[i] == "0":
+                return 0 
+            if i in memory:
+                return memory[i]
+            result = decode(i+1)
+            if i+1<len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+               result = result +  decode(i+2)
+            memory[i] = result
+            return memory[i] 
 
-        def dfs(pos):
-            if pos in memo:
-                return memo[pos]
-            if s[pos] == "0":
-                return 0
-            
-            result = dfs(pos+1)
-            if pos+1<n:
-                if s[pos] == "1":
-                    result = result + dfs(pos+2)
-                elif s[pos] == "2" and s[pos+1] in "0123456":
-                    result = result + dfs(pos+2)
-            memo[pos] = result
-            return memo[pos] 
-        return dfs(0)
+        return decode(0)
+        
+
+# time complexity O(n) 
+# space complexity O(n)
