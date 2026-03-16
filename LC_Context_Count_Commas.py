219ae58b06294bb283d7ba977dba3728741b774d
Shreyash Karandikar <karandikarshreyash@gmail.com>
2026-03-15 23:54:18 -0400

    Add countCommas method to Solution class
    Implemented a method to count commas based on the input number.

--- LC_Context_Count_Commas.py
+++ LC_Context_Count_Commas.py
@@ -1,1 +1,13 @@
-#todo writie code for count commas. 
+class Solution:
+    def countCommas(self, n: int) -> int:
+        result = 0 
+        threshold = 999 
+        multiplier = 1 
+        while n>threshold:
+            result = result + (n - threshold)* multiplier
+            threshold = threshold * 1001 
+            multiplier+=1 
+        return result 
+
+       
+        
