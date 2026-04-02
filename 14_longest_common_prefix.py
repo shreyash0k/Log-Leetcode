5da8ac0ad5a2caaab2815003f071b903315fe1e4
Tanvi Garg <tanvigarg9@Tanvis-MacBook-Air.local>
2026-04-01 23:56:02 -0400

    refactor longest common prefix

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -1,12 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-       
-        for i in range(0,len(strs[0])):
+        result = ""
+        for i in range(len(strs[0])):
             for string in strs:
-                if len(string)<=i or string[i]!=strs[0][i]:
-                    return strs[0][:i] 
-                
+                if i==len(string) or string[i]!=strs[0][i]:
+                    return strs[0][:i]
+          
         return strs[0]
-    
-# tc O(m*n) where m i number of string and n is max lenght of any string 
-# sc is O(m^2) 
