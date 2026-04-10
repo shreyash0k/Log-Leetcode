25369dd4574187b7e6e30ad82ed4685ae0f94a1a
Shreyash <karandikarshreyash@gmail.com>
2026-04-09 23:25:48 -0400

    solve LC 304 Range Sum Query problem

--- 304_Range_Sum_Query_2D_Immutable.py
+++ 304_Range_Sum_Query_2D_Immutable.py
@@ -1,1 +1,25 @@
-# todo: solve this problem 
+class NumMatrix:
+
+    def __init__(self, matrix: list[list[int]]):
+        m, n = len(matrix), len(matrix[0])
+        # prefix[i][j] = sum of rectangle (0,0) -> (i-1, j-1)
+        # extra row/col of zeros simplifies boundary handling
+        self.prefix = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.prefix[i][j] = (
+                    matrix[i-1][j-1]
+                    + self.prefix[i-1][j]
+                    + self.prefix[i][j-1]
+                    - self.prefix[i-1][j-1]
+                )
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        P = self.prefix
+        return (
+            P[row2+1][col2+1]
+            - P[row1][col2+1]
+            - P[row2+1][col1]
+            + P[row1][col1]
+        )
